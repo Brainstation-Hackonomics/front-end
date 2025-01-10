d6f7328bf22bc48560139a0c86ea80320b33274e
Frankie <frankie.m.18@hotmail.com>
2025-01-09 22:17:53 -0800

    modified and added button options, started on profilecard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import "./App.scss";
 import ScotiabankLogo from "./assets/logos/Scotiabank.png";
+import ProfileCard from "./Components/ProfileCard/ProfileCard";
+import Arrow from "../src/assets/images/Arrow.svg";
 
 function App() {
   return (
     <>
-      <img src={ScotiabankLogo} alt="" />
+      <img className="logo" src={ScotiabankLogo} alt="" />
+      <br />
       <div className="test">BUTTON</div>
       <br />
-      <div className="tag-style">Tag</div>
+      <div className="blue-button">Learn More</div>
+      <img src={Arrow} className="blue-button__icon"></img>
+      <div className="tag-style">Learn About This Account</div>
       <p className="card">
         This is just to demonstrate the card border. use @include card-border to
-        use. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        use. <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
         doloremque voluptas blanditiis delectus ex amet earum? Quis accusamus
         voluptate dolores quia eos temporibus itaque debitis rerum, provident
         autem? Exercitationem, modi.
       </p>
+      <ProfileCard></ProfileCard>
     </>
   );
 }
--- src/Components/ProfileCard/ProfileCard.jsx
+++ src/Components/ProfileCard/ProfileCard.jsx
@@ -1,7 +1,18 @@
 import "./ProfileCard.scss";
 
 function ProfileCard() {
-  return <div></div>;
+  return (
+    <figure className="profilecard">
+      <img className="profilecard__image"></img>
+      <h3 className="profilecard__header">Cheryl Smith</h3>
+      <section className="profilecard__tags">
+        tags section
+        <ul className="profilecard__list">
+          <li className="profilecard__items">tag</li>
+        </ul>
+      </section>
+    </figure>
+  );
 }
 
 export default ProfileCard;
