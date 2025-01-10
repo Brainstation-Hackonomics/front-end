3640c1efb68fe2960e0abd52ca06627bf85d1c0f
Laura Courtney <lauraelainecourtney@gmail.com>
2025-01-10 08:54:42 -0400

    Header completed, filter needs to be attached

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import ScotiabankLogo from "./assets/logos/Scotiabank.png";
 import ProfileCard from "./Components/ProfileCard/ProfileCard";
 import Arrow from "../src/assets/images/Arrow.svg";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <img className="logo" src={ScotiabankLogo} alt="" />
       <br />
       <div className="test">BUTTON</div>
