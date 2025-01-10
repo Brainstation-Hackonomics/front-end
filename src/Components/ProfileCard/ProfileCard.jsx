cd8e2ce21921b39ab319a09c057c03f879b18ea5
Frankie <frankie.m.18@hotmail.com>
2025-01-09 23:15:34 -0800

    worked on profile card

--- src/Components/ProfileCard/ProfileCard.jsx
+++ src/Components/ProfileCard/ProfileCard.jsx
@@ -3,12 +3,22 @@ import "./ProfileCard.scss";
 function ProfileCard() {
   return (
     <figure className="profilecard">
-      <img className="profilecard__image"></img>
+      <div className="profilecard__image"></div>
       <h3 className="profilecard__header">Cheryl Smith</h3>
-      <section className="profilecard__tags">
-        tags section
+      <p className="profilecard__body">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+        recusandae minima harum error nihil voluptas eaque atque quae qui sint
+        officiis doloremque eveniet labore quia neque possimus, soluta alias
+        sit.
+      </p>
+      <section className="profilecard__tag-section">
         <ul className="profilecard__list">
-          <li className="profilecard__items">tag</li>
+          <li className="profilecard__items">Tag</li>
+          <li className="profilecard__items">Tag</li>
+          <li className="profilecard__items">Tag</li>
+          <li className="profilecard__items">Tag</li>
+          <li className="profilecard__items">Tag</li>
+          <li className="profilecard__items">Tag</li>
         </ul>
       </section>
     </figure>
